1ab00ea84b655c604836a563acb0f00635217ef7
Morgan Miller <m.miller722@gmail.com>
2015-06-12 08:07:27 -0600

    add some seed data & data types to readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ActiveRecord::Schema.define do
+  create_table :plans do |t|
+    t.string :state
+    t.integer :child_premium
+    t.string :plan_type
+  end
+end
+
+root_dir      = File.expand_path '../', __dir__
+data_filepath = "#{root_dir}/tmp/data.json"
+data = JSON.parse File.read(data_filepath)
+
+column_map = data["meta"]["view"]["columns"]
+               .reject { |c| c["id"] == -1 }
+               .map { |c| [c["name"], c["id"].to_s] }
+               .to_h
+
+data["data"].each do |column|
+  attributes = {
+    state:         column[column_map["State"]],
+    child_premium: column[column_map["Premium Child"]],
+    plan_type:     column[column_map["Plan Type"]],
+  }
+  Plan.find_by(attributes) || Plan.create!(attributes)
+end
--- lib/tasks/scrape.rake
+++ lib/tasks/scrape.rake
@@ -1,8 +1,9 @@
+root_dir      = File.expand_path '../../', __dir__
 data_filepath = "#{root_dir}/tmp/data.json"
+
 
 desc 'Pull the data from the internet, save it to a local file'
 task 'scrape' do
-  require_relative 'services/scrape'
+  require_relative 'scripts/scrape'
   Scrape.call data_filepath
 end
-
